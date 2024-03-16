694047e791a57b3606fd1fa540efc622d2e1ca43
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-03-16 12:55:33 +0100

    Add "about". Add method to prevent loading the same web

--- src/index.js
+++ src/index.js
@@ -3,8 +3,34 @@ import homepage from './pages/home.js';
 import about from './pages/about.js';
 
 (function () {
-    // homepage();
-    const btnHome = document.querySelector("button.home");
-    btnHome.addEventListener("click", homepage);
-    about();
+
+    const lastButtonCalled = ["home"];
+    const buttonListClass = ['home', 'menu', 'delivery', 'about'];
+    const funcsList = [homepageCall, aboutCall, homepageCall, aboutCall];
+    homepage();
+    buttonListClass.forEach((e, index) => {
+        const btn = document.querySelector(`button.${e}`);
+        btn.addEventListener("click", funcsList[index]);
+    })
+
+    // We only load a new script, we do not load the same script again and again
+    function homepageCall(e) {
+        if (checkLastCall(e)) return;
+        homepage();
+    }
+
+    function aboutCall(e) {
+        if (checkLastCall(e)) return;
+        about();
+    }
+
+    function checkLastCall(e) {
+        if (lastButtonCalled[0] === e.target.classList[0])
+            return true;
+        else {
+            lastButtonCalled[0] = e.target.classList[0];
+            return false;
+        }
+    }
+
 })();
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,39 @@
-import Image from "../images/chef_mhw.jpg";
+import image from "../images/chef_mhw.jpg";
 
-export default function() {
-    const img = document.querySelector("img"); 
-    img.src = Image;
-    console.log(img);
+export default function about() {
+
+//            <div id="about">Félix House: where authentic food is made~</div>
+//            <div class="about-text"><p>At this restaurant you can try dishes as old as Earth itself and as new as the latest Javascript framework. From Spanish food, American food, to even Asian food. Our head chef is able to just nail every single plate he put his mind on. We guarantee you, you will try to come more than once. That is, if your wallet allows it.</p>
+//                <p>Without further ado here it is the kitchen crew!</p></div>
+//            <div><img id="chef" alt="Picture of our chef"></div>
+    const contentDiv = document.querySelector("#content");
+
+    while(contentDiv.firstChild) {
+        contentDiv.removeChild(contentDiv.firstChild);
+    }
+
+    let tempDiv = document.createElement("div");
+    tempDiv.id = "about";
+    tempDiv.textContent = "Félix House: where authentic food is made~";
+    contentDiv.appendChild(tempDiv);
+
+    tempDiv = document.createElement("div");
+    tempDiv.classList.add("about-text");
+    let tempParagraph = document.createElement("p");
+    tempParagraph.textContent = "At this restaurant you can try dishes as old as Earth itself and as new as the latest Javascript framework. From Spanish food, American food, to even Asian food. Our head chef is able to just nail every single plate he put his mind on. We guarantee you, you will try to come more than once. That is, if your wallet allows it.";
+    tempDiv.appendChild(tempParagraph)
+    tempParagraph = document.createElement("p");
+    tempParagraph.textContent = "Without further ado here it is the kitchen crew!";
+    tempDiv.appendChild(tempParagraph);
+    contentDiv.appendChild(tempDiv);
+
+    tempDiv = document.createElement("div");
+    const imgElement = document.createElement("img"); 
+    imgElement.src = image;
+    imgElement.id = "chef"; 
+    imgElement.alt = "Picture of our chef";
+    tempDiv.appendChild(imgElement);
+    contentDiv.appendChild(tempDiv);
 }
+
+
