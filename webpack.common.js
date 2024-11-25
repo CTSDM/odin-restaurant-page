21f549565eb9fbfb3f62c087d74f277eaf224347
CTSDM <156813428+CTSDM@users.noreply.github.com>
2024-11-25 17:04:21 +0900

    Modify webpack configuration to handle different modes

--- webpack.common.js
+++ webpack.common.js
@@ -2,19 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        watchFiles: {
-            paths: ['src/index.html', 'src/style.css'],
-        },
     },
     module: {
         rules: [
